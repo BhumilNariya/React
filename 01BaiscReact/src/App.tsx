e2fa76da3807d3ab4ce3af13a249a400c46872ac
BhumilNariya <12302080601010@adit.ac.in>
2025-12-25 18:19:46 +0530

    prctice commit

--- 01BaiscReact/src/App.tsx
+++ 01BaiscReact/src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import Cart from './App2'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Hello React Sutdy with Me.</h1>
+    <>
+    <Cart/>   
+    <h1>Hello React Sutdy with Me.</h1> 
+    </>
+    
   )
 }
 
---
+++ 01BaiscReact/src/App2.tsx
@@ -0,0 +1,11 @@
+function Cart(){
+    return(
+        <>
+        <h1>Hello day 2 of react learningggg...</h1>
+        </>
+    )
+}
+
+export  default Cart
+
+
