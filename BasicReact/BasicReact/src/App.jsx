fd6640a7acbc28775181a98ff0851024980b73ba
BhumilNariya <12302080601010@adit.ac.in>
2025-12-25 21:49:17 +0530

    Learn How Hook Work

--- BasicReact/BasicReact/src/App.jsx
+++ BasicReact/BasicReact/src/App.jsx
@@ -4,30 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+
+
+   let [counter,setCounter]= useState(15)
+
+
+  const addValue=()=>{
+      console.log("Cilcked",counter);
+      setCounter(counter+1);
+  }
+  const removeValue=()=>{
+      console.log("Cilcked",counter);
+      setCounter(counter-1);
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Bhumil Nariya</h1>
+      <h2>Counter Value is :{counter}</h2>
+      <button onDoubleClick={addValue}>Like Post:{counter}</button>
+      <button onClick={removeValue}>Dislike Post:{counter}</button>
+      
     </>
   )
 }
