9d711635585854c1ec816d129f14db082ea7df8f
BhumilNariya <12302080601010@adit.ac.in>
2025-12-26 21:32:02 +0530

    Props

--- TailwindCssAndProps/project/src/App.jsx
+++ TailwindCssAndProps/project/src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './Components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
-
+ const myObj={
+      name:"bhumil",
+      age:21
+ }
   return (
     <>
-      <h1>PHOENIX</h1>
+      <h1 className='bg-green-500 text-black rounded-xl mb-3'>PHOENIX</h1>
+      <Card username="bhumilNariya" btnText="Click Me!!" someObj={myObj}/>
+      <br />
+      <Card username="PHOENIX" btnText="Visit Me!!" someObj={myObj}/>
     </>
   )
 }
