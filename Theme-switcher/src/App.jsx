3a544cf1c86d7137d750380b0a658fac057b3b63
BhumilNariya <12302080601010@adit.ac.in>
2025-12-31 10:33:24 +0530

    Complete Project

--- Theme-switcher/src/App.jsx
+++ Theme-switcher/src/App.jsx
@@ -1,39 +1,42 @@
-import { useState,useEffect } from 'react'
-import ThemeProvider from './context/Theme'
-import ThemeBtn from './Components/ThemeBtn'
-import Card from './Components/Card'
+import { useEffect, useState } from 'react'
+import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
-  const [themeMode , setThemeMode]=useState("light")
+  const [themeMode, setThemeMode] = useState("light")
 
-  const lightTheme=()=>{
+  const lightTheme = () => {
     setThemeMode("light")
   }
-  const darkTheme=()=>{
+
+  const darkTheme = () => {
     setThemeMode("dark")
   }
 
-  useEffect(()=>{
-    document.querySelector('html').classList.remove("light","dark")
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
     document.querySelector('html').classList.add(themeMode)
-  },[themeMode])
+  }, [themeMode])
+  
 
   return (
-    <>
-      <ThemeProvider value={{themeMode, lightTheme,darkTheme}} >
-            <div className="flex flex-wrap min-h-screen items-center">
-                <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        <ThemeBtn />
-                    </div>
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                  <ThemeBtn />
+              </div>
 
-                    <div className="w-full max-w-sm mx-auto">
-                       <Card />
-                    </div>
-                </div>
-            </div>
-       </ThemeProvider>
-    </>
+              <div className="w-full max-w-sm mx-auto">
+                  <Card />
+              </div>
+          </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
