aaea5a2db7f90752bcc9c212846e9ab00914edf3
BhumilNariya <12302080601010@adit.ac.in>
2026-01-04 16:11:13 +0530

    KeyPress

--- UnlimitedColor/app.js
+++ UnlimitedColor/app.js
@@ -1,28 +1,53 @@
 //generate hex code
-
 const randomColor=function(){
-    const hex="0987654321ABCDEF";
-    let color ="#";
-    for (let i = 0; i <6; i++) {
-     color +=hex[Math.floor(Math.random()*16)]
+    const hex="1234567890ABCDEF";
+    let color="#"
+
+    for (let i = 0; i < 6; i++) {
+        color +=hex[Math.floor(Math.random()*16)]
+        
     }
-    return color;
+    return color
 }
-let intervalId
-const startingChangingColor=function (){
 
-        if(!intervalId){
-            intervalId= setInterval(changeColor,1000)
-        }
 
+
+let intervalId;
+const StartChangingColor=function (){
+    
+    intervalId=setInterval(changeColor,1000)
+    
     function changeColor(){
         document.body.style.backgroundColor=randomColor();
     }
-
 }
-const stopChangingColor=function (){
+const StopChangingColor=function (){
     clearInterval(intervalId)
-
 }
-document.querySelector("#start").addEventListener("click",startingChangingColor)
-document.querySelector("#stop").addEventListener("click",stopChangingColor)
+
+
+
+document.querySelector("#start").addEventListener("click",StartChangingColor)
+document.querySelector("#stop").addEventListener("click",StopChangingColor)
+
+const insert=document.querySelector("#insert")
+
+window.addEventListener("keydown",(e)=>{
+    insert.innerHTML=`
+        <div>
+         <table>
+  <tr>
+    <th>key</th>
+    <th>keycode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+  
+</table> 
+        </div>
+    `
+})
