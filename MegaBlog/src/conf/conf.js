1a247fbc2b9c41778f06fa625afd076b02cd27d2
BhumilNariya <12302080601010@adit.ac.in>
2026-01-21 14:17:36 +0530

    Configuration of File upload and Custom Queries

--- MegaBlog/src/conf/conf.js
+++ MegaBlog/src/conf/conf.js
@@ -3,6 +3,8 @@ const conf={
     appwriteProjectId:String(import.meta.env.VITE_PROJECT_ID),
     appwriteCollectionId:String(import.meta.env.VITE_COLLECTION_ID),
     appwriteBucketId:String(import.meta.env.VITE_BUCKET_ID),
+    appwriteDatabaseId:String(import.meta.env.VITE_DATABASE_ID),
+
 }
 
 export default conf
