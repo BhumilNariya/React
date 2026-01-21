23526e66aa9ddfdf78e0e24e63e7db47dd252c60
BhumilNariya <12302080601010@adit.ac.in>
2026-01-21 19:27:38 +0530

    Confiuration of redux ToolKit

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,17 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import {useDispatch} from "react-redux"
+import authService from "./appwrite/auth"
+import {login , logout } from "./store/authSlice"
+import {Header,Footer} from "./components"
+import {Outlet} from "react-router-dom"
+import "./App.css";
 
 function App() {
   // console.log(process.env.REACT_APP_APPWRITE_URL) // BCZ of using React  not run this
-  console.log(import.meta.env.VITE_APPWRITE_URL)  //BCz of using Vite
+  console.log(import.meta.env.VITE_APPWRITE_URL); //BCz of using Vite
 
-  return (
-    <>
-      <h1>Mega Blog with AppWrite</h1>
-    </>
-  )
+  const [loading,setLoading]=useState(true)
+  const dispatch=useDispatch()   
+  
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if (userData) {
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout())
+      }
+    })
+    .finally( ()=>setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className="min-h- flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+            ToDo: <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
-export default App
+export default App;
