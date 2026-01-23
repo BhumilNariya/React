6184231439cda71e36298864cbb4c7b7d84453e4
BhumilNariya <12302080601010@adit.ac.in>
2026-01-23 18:03:05 +0530

    Pages

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -4,11 +4,23 @@ import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
 import RTE from "./RTE";
+import Signup from "../pages/Signup";
+import Login from "../pages/Login";
+import AddPost from "../pages/AddPost";
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
 
 export {Header,
     Footer,
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    AddPost,
+    PostForm,
+    PostCard
+
+
 }
